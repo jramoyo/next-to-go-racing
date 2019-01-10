0b520d15fca20831c6680cbf86bd34e30bf1ba0b
Jan Amoyo <jramoyo@gmail.com>
2019-01-10 18:53:17 +1100

    calls next-to-go API through proxy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
+
+  state = { races: [] }
+
+  componentDidMount = () => {
+    axios.get('/v1/tab-info-service/racing/next-to-go/races?jurisdiction=NSW&raceType=H')
+      .then((res) => {
+        console.log(res.data.races);
+        this.setState({races: res.data.races})
+      });
+  }
+
   render() {
+    const rows = this.state.races.map(race => {
+        return (
+          <tr key={race.raceName + '_' + race.raceStartTime}>
+            <td>name</td>
+            <td>{race.raceName}</td>
+          </tr>
+        )
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
       </div>
     );
   }
